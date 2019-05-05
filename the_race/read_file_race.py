6beaab0690c09cadcf287266572d2aa3e71046e9
Victor Pereira <victtor.vpb@gmail.com>
2019-05-05 14:38:49 -0300

    refactory class read_file_race

--- the_race/read_file_race.py
+++ the_race/read_file_race.py
@@ -1,22 +1,28 @@
+import re
 import os.path
 
 
 class ReadFileRace(object):
     def __init__(self, file_path):
-        if not os.path.isfile(file_path):
+        if not os.path.isfile(file_path,):
             raise FileExistsError(f"File {file_path} not exist")
 
-        with open(file_path) as file:
-            self.read_data = file.read()
+        self.file_path = file_path
 
     @property
     def to_list(self):
-        race_data = []
-
-        for line in self.read_data.splitlines()[1:]:
-            line = line.split()
-            line.remove(line[2])
-
-            race_data.append(line)
-
-        return race_data
+        with open(self.file_path, "r", encoding="utf-8") as file:
+            laps = []
+            for i, line in enumerate(file):
+                lap = {}
+                if(i != 0):
+                    # import pdb; pdb.set_trace()
+                    match = re.findall('[\w.:]+', line)
+                    lap["time"] = match[0]
+                    lap["id"] = match[1]
+                    lap["name"] = match[2]
+                    lap["number"] = match[3]
+                    lap["lap_time"] = match[4]
+                    lap["avg_speed"] = match[5]
+                    laps.append(lap)
+        return laps
