d79a52ed58248ef09e9b5b325adb6f7317a368a6
Victor Pereira <victtor.vpb@gmail.com>
2019-05-05 16:02:05 -0300

    process data the race

---
+++ the_race/tests_the_race.py
@@ -0,0 +1,25 @@
+import unittest
+
+import datetime as dt
+
+from .read_file_race import ReadFileRace
+from .the_race import TheRace
+from .test_variables import var_process_data, list_total_time
+
+
+class TheRaceTests(unittest.TestCase):
+
+    def setUp(self):
+        self.list_laps = ReadFileRace('race.log').to_list
+        self.the_race = TheRace(self.list_laps)
+
+    def test_process_datas(self):
+        self.assertEqual(self.the_race.process_data(), var_process_data)
+    
+    def test_total_time(self):
+
+        total_time = total_time = dt.timedelta(
+            seconds=251,
+            microseconds=57800
+        )
+        self.assertEqual(self.the_race.calculate_total_time_pilot(list_total_time), total_time)
